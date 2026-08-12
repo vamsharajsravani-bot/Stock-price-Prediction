9f6f3a5f4feb3549d7748cb02d74a1b667ef7361
R SRAVANI <vamsharajsravani@gmail.com>
2026-08-12 15:26:26 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('predictor.urls')),
+    path('', views.index, name='index'),
+    path('predict/', views.predict, name='predict'),
 ]
